8a38c4adaa90b3d78cc6ee7987eb70a2f3f322f5
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-10-25 10:05:44 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 export default function App() {
   const [name, setName] = useState();
-
   const [age, setAge] = useState();
-
   const [city, setCity] = useState();
-
   const [state, setState] = useState();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    alert("Fill the fields");
+  }, []);
 
   function handleShowInfo() {
-    console.log(`Name: ${name}, Age: ${age}, City: ${city}, State: ${state}`);
+    alert(`Name: ${name}, Age: ${age}, City: ${city}, State: ${state}`);
   }
 
   return (
     <div>
-      <form>
-        <input
-          placeholder="Type your name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          placeholder="Type your age "
-          onChange={(e) => setAge(e.target.value)}
-        />
-        <input
-          placeholder="Type your city"
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <input
-          placeholder="Type your state "
-          onChange={(e) => setState(e.target.value)}
-        />
+      <div class="container">
+        <form>
+          <input
+            placeholder="Type your name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            placeholder="Type your age "
+            onChange={(e) => setAge(e.target.value)}
+          />
+          <input
+            placeholder="Type your city"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <input
+            placeholder="Type your state "
+            onChange={(e) => setState(e.target.value)}
+          />
+        </form>
+        <button onClick={handleShowInfo}>Show info</button>
 
-        <p>{name}</p>
-        <p>{age}</p>
-        <p>{city}</p>
-        <p>{state}</p>
-      </form>
-      <button onClick={handleShowInfo}>Show info</button>
+        <p> You clicked {count} time(s)</p>
+        <button onClick={() => setCount(count + 1)}>Click here</button>
+      </div>
     </div>
   );
 }
